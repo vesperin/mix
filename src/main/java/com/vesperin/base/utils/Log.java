6bb955bac5fe8ccf9dfc207b11c51ac7babfc130
hsanchez <huascar.sanchez@gmail.com>
2017-10-20 18:03:58 -0700

    monitoring api

---
+++ src/main/java/com/vesperin/base/utils/Log.java
@@ -0,0 +1,96 @@
+package com.vesperin.base.utils;
+
+import java.io.PrintStream;
+
+/**
+ * @author Huascar Sanchez
+ */
+public class Log {
+  private final boolean silenced;
+  private PrintStream   out;
+
+  /**
+   * Construct a Log or log viewer using
+   * System.out as a print stream object.
+   */
+  private Log(){
+    this(false, System.out);
+  }
+
+  /**
+   * Construct a Log or log viewer using
+   * a print stream object.
+   *
+   * @param silenced silence log output
+   * @param out the current print stream
+   */
+  private Log(boolean silenced, PrintStream out){
+    this.silenced = silenced;
+    this.out      = out;
+  }
+
+  /**
+   * @return a new Log that silences its output.
+   */
+  public static Log quiet(){
+    return new Log(true, System.out);
+  }
+
+  /**
+   * @return a new Log that logs its output.
+   */
+  public static Log verbose(){
+    return new Log();
+  }
+
+  /**
+   * Logs an error caused by some throwable.
+   *
+   * @param s the error label
+   * @param exception the actual error
+   */
+  public void error(String s, Throwable exception){
+    if(!silenced){
+      out.println("ERROR: " + s);
+      exception.printStackTrace(System.err);
+    }
+  }
+
+  /**
+   * Logs some important information
+   *
+   * @param s the important information
+   */
+  public void info(String s){
+    if(!silenced){
+      out.println("INFO: " + s);
+    }
+  }
+
+  /**
+   * Checks if Log is a chatter box.
+   *
+   * @return true if it is in verbose mode; false otherwise.
+   */
+  public boolean isVerbose(){
+    return !silenced;
+  }
+
+  /**
+   * Logs a message without assigning a proper label and adding a line break.
+   *
+   * @param s message.
+   */
+  public void log(String s){
+    if (!silenced) out.print(s);
+  }
+
+  /**
+   * Logs a warning message
+   *
+   * @param s the warning message
+   */
+  public void warn(String s){
+    if (!silenced) out.println("WARN: " + s);
+  }
+}
---
+++ src/main/java/com/vesperin/base/utils/StopWatch.java
@@ -0,0 +1,47 @@
+package com.vesperin.base.utils;
+
+import java.util.Objects;
+
+/**
+ * Measures the time that elapses between the start and end of a
+ * programming task (wall-clock time).
+ */
+public final class StopWatch {
+
+  private long start = System.currentTimeMillis();
+  private static Log logger = Log.quiet();
+
+  public static StopWatch make(){
+    return make(null);
+  }
+
+  public static StopWatch make(Log log){
+    if(!Objects.isNull(log)){
+      logger = log;
+    }
+
+    return new StopWatch();
+  }
+
+  /**
+   * Resets and returns elapsed time in milliseconds.
+   */
+  private double reset() {
+    long now = System.currentTimeMillis();
+    try {
+      return (now - start)/1000.0;
+    } finally {
+      start = now;
+    }
+  }
+
+
+  /**
+   * Resets and then logs the elapsed CPU time (in seconds)
+   * since the stopwatch was created.
+   *
+   */
+  public void elapsedTime(String label) {
+    logger.info(label + ": " + reset() + "s");
+  }
+}
