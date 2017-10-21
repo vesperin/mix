ab1c0c6271105f52b029c6728276e0687ab6e7dd
hsanchez <huascar.sanchez@gmail.com>
2017-10-20 21:00:55 -0700

    threads and tasks

---
+++ src/main/java/com/vesperin/tasks/Task.java
@@ -0,0 +1,135 @@
+package com.vesperin.tasks;
+
+import com.vesperin.utils.Log;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Huascar Sanchez
+ */
+abstract class Task {
+  private final String name;
+
+  final List<Task> firstToFinish              = new ArrayList<>();
+  final List<Task> firstToSuccessfullyFinish  = new ArrayList<>();
+
+  volatile TaskResult result;
+
+  Exception thrown;
+
+
+  /**
+   * Construct a new task object.
+   *
+   * @param name the name of the task object.
+   */
+  Task(String name){
+    this.name = name;
+  }
+
+  /**
+   * Adds a task that must be completed before this task.
+   *
+   * @param prerequisite the pre requisite task.
+   * @return self
+   */
+  Task after(Task prerequisite){
+    firstToFinish.add(Objects.requireNonNull(prerequisite));
+    return this;
+  }
+
+  /**
+   * Adds a collection of tasks that must be completed before this task.
+   *
+   * @param prerequisites the collection of pre requisites
+   * @return self
+   */
+  Task after(Collection<Task> prerequisites){
+    prerequisites.forEach(this::after);
+    return this;
+  }
+
+  /**
+   * Adds a successful task that must be completed before this task.
+   *
+   * @param prerequisite the pre requisite and successful task.
+   * @return self
+   */
+  Task afterSuccess(Task prerequisite){
+    firstToSuccessfullyFinish.add(Objects.requireNonNull(prerequisite));
+    return this;
+  }
+
+  /**
+   * Adds a collection of successful tasks that must be completed before this task.
+   *
+   * @param prerequisites the collection of pre requisites and successful tasks.
+   * @return self
+   */
+  Task afterSuccess(Collection<Task> prerequisites){
+    prerequisites.forEach(this::afterSuccess);
+    return this;
+  }
+
+  protected abstract TaskResult execute() throws Exception;
+
+  /**
+   * @return true if this is an action task. A task queue imposes certain
+   * limits on how many actions may be run concurrently.
+   */
+  boolean isAction(){
+    return false;
+  }
+
+
+  /**
+   * @return true if this is a runnable task; false otherwise.
+   */
+  final boolean isRunnable(){
+
+    for(Task each : firstToFinish){
+      if(each.result == null){
+        return false;
+      }
+    }
+
+    for(Task each : firstToSuccessfullyFinish){
+      if(each.result != TaskResult.SUCCESS){
+        return false;
+      }
+    }
+
+
+    return true;
+  }
+
+  /**
+   * Runs this task.
+   *
+   * @param log output log.
+   */
+  final void run(Log log){
+    if(!Objects.isNull(result)) throw new IllegalStateException();
+
+    try {
+      log.info("Running " + this);
+      result  = execute();
+    } catch (Exception e){
+      log.info(name + " failed");
+
+      thrown  = e;
+      result  = TaskResult.ERROR;
+    }
+
+    if(TaskResult.SUCCESS != result){
+      log.warn(this + " " + result);
+    }
+  }
+
+  @Override public String toString() {
+    return name;
+  }
+}
---
+++ src/main/java/com/vesperin/tasks/Threads.java
@@ -0,0 +1,49 @@
+package com.vesperin.tasks;
+
+import com.vesperin.utils.Expect;
+import com.vesperin.utils.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Huascar Sanchez
+ */
+public class Threads {
+  private Threads(){}
+
+  public static ExecutorService threadPerCpuExecutor(Log log, String name) {
+    Expect.validArgument(name != null && !name.isEmpty());
+
+    return fixedThreadsExecutor(log, name, Runtime.getRuntime().availableProcessors());
+  }
+
+  private static ExecutorService fixedThreadsExecutor(final Log log, String name, int count) {
+    ThreadFactory threadFactory = daemonThreadFactory(name);
+
+    return new ThreadPoolExecutor(count, count, 10, TimeUnit.SECONDS,
+      new LinkedBlockingQueue<>(Integer.MAX_VALUE), threadFactory) {
+
+      @Override protected void afterExecute(Runnable runnable, Throwable throwable) {
+        if (throwable != null) {
+          log.error("Unexpected failure from " + runnable, throwable);
+        }
+      }
+    };
+  }
+
+  private static ThreadFactory daemonThreadFactory(final String name) {
+    return new ThreadFactory() {
+      private int nextId = 0;
+
+      public synchronized Thread newThread(Runnable r) {
+        Thread thread = new Thread(r, name + "-" + (nextId++));
+        thread.setDaemon(true);
+        return thread;
+      }
+    };
+  }
+}
