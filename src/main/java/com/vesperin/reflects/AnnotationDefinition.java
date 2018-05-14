d2833dd3db2b3cb3372da1652b669848f1e5836a
Zhuo Chen <zhuochen@Zhuos-MacBook-Pro.local>
2018-05-14 16:12:08 -0400

    Parsing member values of an annotation.

--- src/main/java/com/vesperin/reflects/AnnotationDefinition.java
+++ src/main/java/com/vesperin/reflects/AnnotationDefinition.java
@@ -6,13 +6,20 @@ import org.eclipse.jdt.core.dom.IMemberValuePairBinding;
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Huascar Sanchez
  */
 public class AnnotationDefinition {
   private final String value;
+
+  private final Map<String, Set<String>> memberValues;
 
   /**
    * Creates a new annotation definition.
@@ -29,7 +36,12 @@ public class AnnotationDefinition {
    * @param value the value of the {@link Annotation} object.
    */
   public AnnotationDefinition(final String value) {
+    this(value, new HashMap<>());
+  }
+
+  private AnnotationDefinition(final String value, Map<String, Set<String>> memberValues) {
     this.value = value;
+    this.memberValues = memberValues;
   }
 
   static AnnotationDefinition annotationDefinition(IAnnotationBinding annotationBinding){
@@ -37,6 +49,8 @@ public class AnnotationDefinition {
     annotation.append("(");
 
     final List<String> entries = new ArrayList<>();
+
+    final Map<String, Set<String>> memberValues = new HashMap<>();
 
     for(IMemberValuePairBinding each : annotationBinding.getDeclaredMemberValuePairs()){
       final String key = each.getName();
@@ -46,8 +60,11 @@ public class AnnotationDefinition {
       if(valObj != null && valObj.getClass().isArray()){
         final Object[] arrayObj = (Object[]) valObj;
         val = Arrays.toString(arrayObj);
+
+        memberValues.put(key, Arrays.asList(arrayObj).stream().map(Object::toString).collect(Collectors.toSet()));
       } else {
         val = String.valueOf(valObj);
+        memberValues.put(key, new HashSet<>(Arrays.asList(val)));
       }
       final String entry = key + "=" + val;
       entries.add(entry);
@@ -58,10 +75,14 @@ public class AnnotationDefinition {
 
     annotation.append(")");
 
-    return new AnnotationDefinition(annotation.toString());
+    return new AnnotationDefinition(annotation.toString(), memberValues);
   }
 
   @Override public String toString() {
     return this.value;
+  }
+
+  public Map<String, Set<String>> getMemberValues() {
+    return memberValues;
   }
 }
